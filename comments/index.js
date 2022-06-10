7766c76281c93b3fdb5eefdac33fe4dcdd03bb1c
shaheerkp <kpshaheer123@gmail.com>
2022-06-11 02:21:57 +0530

    first commit

---
+++ comments/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const { randomBytes } = require('crypto')
+
+const cors=require('cors')
+const axios=require('axios')
+
+
+const app = express()
+
+app.use(express.json())
+app.use(cors({limit:'50mb'}))
+
+
+const commentsByPostId = {};
+
+
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostId[req.params.id]||[])
+
+})
+
+
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex')
+    const {content}=req.body;
+    const comments=commentsByPostId[req.params.id]||[]
+
+    comments.push({id:commentId,content})
+    commentsByPostId[req.params.id]=comments
+    res.status(201).send(comments);
+
+
+
+   
+    res.status(201).send(posts[id])
+
+})
+
+app.listen(4001, () => {
+    console.log("listening on 4001");
+})
